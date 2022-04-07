df4954f4c6ea7cf16af21df0799cc065b5d86a97
FraAccaBi <francescahuda@gmail.com>
2022-04-07 15:53:22 +0200

    BONUS 1commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,8 @@ for (let index = 0; index < team.length; index++) {
     //associo al div sopra il contenuto di addetto
     addettoElement.innerHTML = addetto.name + ' | '
     addettoElement.innerHTML += addetto.role + ' | '
-    addettoElement.innerHTML += addetto.image
+    addettoElement.innerHTML += '' + addetto.image
+    addImg(addetto.image, addettoElement)
 
     //appendo all'div generico team nel dom l'addettoElement
     teamElement.append(addettoElement)
@@ -65,5 +66,25 @@ for (let index = 0; index < team.length; index++) {
     
 }
 
+function addImg(objValue,element) {
+    let img = document.createElement('img')
+    img.src = `./img/${objValue}` 
+    console.log(img.src);  
+    element.append(img) 
+}
 
-
+/* var up = document.getElementById('GFG_UP'); 
+        up.innerHTML = "Click on the button to add image element"; 
+        var down = document.getElementById('GFG_DOWN'); 
+          
+        function GFG_Fun() {
+            var img = document.createElement('img');
+            img.src = 
+'https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png';
+            document.getElementById('body').appendChild(img);
+            down.innerHTML = "Image Element Added."; 
+        }
+        
+        
+<img src="./img/angela-caroll-chief-editor.jpg" alt="">
+*/
